2513c94827100490256e4b7f7ef83868f57193ee
Subidubi12 <luisfslara@gmail.com>
2025-07-19 17:38:39 -0600

    instrucciones

---
+++ src/app/instrucciones/page.tsx
@@ -0,0 +1,19 @@
+export default function Instrucciones(){
+    return (
+        <>
+            <title>
+                Istrucciones para que les jale esta cosa xd
+            </title>
+            <p>
+                Crear una carpeta dentro de src\app :<br />
+                y dentro de ella un archivo llamado "page.tsx" :<br />
+                srcapp\"nombreCarpeta"\page.tsx  :<br />
+
+                el nombre de la carpeta es el componente que van a hacer :<br />
+            </p>
+
+        </>
+    );
+
+
+}
