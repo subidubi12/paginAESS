024f16a21ab9586de13acbfbfc840f638d477ec2
Subidubi12 <luisfslara@gmail.com>
2025-07-19 14:46:43 -0600

    Subida de archivos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            VAMOS A COMENZAR CON LAS TAREAS{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            HAREMOS USO DE STYLED COMPONENTS
           </li>
         </ol>
 
